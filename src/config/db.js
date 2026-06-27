0e97e7ce128c1f317022cfcf8d197b26c98dd7f8
Deep <playstore1392004@gmail.com>
2026-06-27 20:20:45 +0530

    Result Fixed

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,8 @@
+const pg = require('pg');
 const knex = require('knex');
 const config = require('../../knexfile');
+
+pg.types.setTypeParser(pg.types.builtins.NUMERIC, parseFloat);
 
 const db = knex(config.development);
 
